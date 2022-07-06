39a66850614cb26406c0ee73c5c5ba181b3c9d9f
elmi-elmi <shahrokhelmi@gmail.com>
2022-07-06 20:15:37 +0430

    delete task

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque esse nihil quo! Accusamus illo illum iure laudantium nam quae unde vel! Accusantium deleniti deserunt laboriosam molestias quis vel. Ipsum, labore, nostrum! Alias consequuntur cum dolor eaque ex pariatur, praesentium?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum cumque enim est et eveniet labore, nesciunt, numquam perspiciatis rem vitae.</p>
   </div>
 </template>
---
+++ src/views/ViewTodo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+
+
+    <v-list
+        flat
+        class="pt-0"
+    >
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item
+            :class="{'green lighten-5':task.done}"
+            @click="doneTask(task)"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through':task.done}"
+              >{{ task.todo }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action
+            @click.stop="deleteTask(task.id)"
+            >
+              <v-btn icon>
+                <v-icon color="grey lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ViewTodo',
+  data() {
+    return {
+      tasks: [
+        {id: 1, todo: 'Wake up', done: false},
+        {id: 2, todo: 'Do nothing', done: true},
+        {id: 3, todo: 'Eat', done: true},
+        {id: 4, todo: 'Enough for today', done: false},
+      ]
+    }
+  },
+  methods: {
+    doneTask(e) {
+      e.done = !e.done
+    },
+    deleteTask(id){
+      this.tasks=  this.tasks.filter(t=>t.id !== id)
+    }
+
+  }
+
+}
+</script>
